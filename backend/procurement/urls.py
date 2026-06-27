1bee21b5601beeecc5a99308d7b81acae49b90c6
sameer9860 <samirkhatiwada68@gmail.com>
2026-06-27 14:16:49 +0545

    feat : update url for vendor

--- backend/procurement/urls.py
+++ backend/procurement/urls.py
@@ -1,10 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import PurchaseRequestViewSet
+from .views import PurchaseRequestViewSet, VendorViewSet, VendorCategoryViewSet
 
 router = DefaultRouter()
 router.register('requests', PurchaseRequestViewSet, basename='purchase-request')
+router.register('vendors', VendorViewSet, basename='vendor')
+router.register('vendor-categories', VendorCategoryViewSet, basename='vendor-category')
 
 urlpatterns = [
     path('', include(router.urls)),
+
 ]
