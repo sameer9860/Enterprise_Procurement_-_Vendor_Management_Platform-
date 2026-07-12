bc1838947ba9651d1b0fc1f17e3b2874381c5408
sameer9860 <samirkhatiwada68@gmail.com>
2026-07-12 20:05:41 +0545

    Feat: Add InvoiceFilter class to filters.py

--- backend/procurement/filters.py
+++ backend/procurement/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import PurchaseRequest, PurchaseOrder
+from .models import PurchaseRequest, PurchaseOrder, Invoice
 
 
 class PurchaseRequestFilter(django_filters.FilterSet):
@@ -27,3 +27,28 @@ class PurchaseOrderFilter(django_filters.FilterSet):
     class Meta:
         model = PurchaseOrder
         fields = ['status', 'vendor']
+
+
+class InvoiceFilter(django_filters.FilterSet):
+    status = django_filters.ChoiceFilter(choices=Invoice.Status.choices)
+    vendor = django_filters.NumberFilter(field_name='vendor__id')
+    min_amount = django_filters.NumberFilter(field_name='amount', lookup_expr='gte')
+    max_amount = django_filters.NumberFilter(field_name='amount', lookup_expr='lte')
+    due_before = django_filters.DateFilter(field_name='due_date', lookup_expr='lte')
+    due_after = django_filters.DateFilter(field_name='due_date', lookup_expr='gte')
+    submitted_after = django_filters.DateFilter(field_name='submitted_at', lookup_expr='gte')
+    overdue = django_filters.BooleanFilter(method='filter_overdue')
+
+    class Meta:
+        model = Invoice
+        fields = ['status', 'vendor']
+
+    def filter_overdue(self, queryset, name, value):
+        from django.utils import timezone
+        today = timezone.now().date()
+        if value:
+            return queryset.filter(
+                due_date__lt=today,
+                status__in=['SUBMITTED', 'UNDER_REVIEW', 'APPROVED']
+            )
+        return queryset
