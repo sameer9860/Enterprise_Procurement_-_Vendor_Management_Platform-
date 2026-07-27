83926ef1a8e3bd9b9b0d31bfdb364a397cf5d628
sameer9860 <samirkhatiwada68@gmail.com>
2026-07-27 19:51:34 +0545

    Feat(accounts): Wire ThrottledTokenObtainPairView to /token/ URL

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProfileView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, ProfileView, ThrottledTokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', ThrottledTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
